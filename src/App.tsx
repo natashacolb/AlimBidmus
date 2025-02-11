cebd9594133bffdb2e759155bb81cfa507492659
natashacolb <natasha.colbt@yahoo.com>
2025-02-11 18:10:54 +0000

    chang

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import { useRoutes, Routes, Route } from "react-router-dom";
+import { useRoutes, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/home";
 import routes from "tempo-routes";
 
@@ -8,111 +8,27 @@ function App() {
     <Suspense fallback={<p>Loading...</p>}>
       <>
         <Routes>
-          <Route path="/" element={<Home initialUserType="care-worker" />} />
+          {/* Root redirect */}
+          <Route
+            path="/"
+            element={<Navigate to="/careworkerdashboard" replace />}
+          />
 
           {/* Care Worker Routes */}
           <Route
-            path="/careworkerdashboard"
-            element={<Home initialUserType="care-worker" />}
-          />
-          <Route
-            path="/careworkerdashboard/shifts"
-            element={<Home initialUserType="care-worker" />}
-          />
-          <Route
-            path="/careworkerdashboard/bookings"
-            element={<Home initialUserType="care-worker" />}
-          />
-          <Route
-            path="/careworkerdashboard/availability"
-            element={<Home initialUserType="care-worker" />}
-          />
-          <Route
-            path="/careworkerdashboard/timesheet"
-            element={<Home initialUserType="care-worker" />}
-          />
-          <Route
-            path="/careworkerdashboard/payslips"
-            element={<Home initialUserType="care-worker" />}
-          />
-          <Route
-            path="/careworkerdashboard/report"
-            element={<Home initialUserType="care-worker" />}
-          />
-          <Route
-            path="/careworkerdashboard/settings"
+            path="/careworkerdashboard/*"
             element={<Home initialUserType="care-worker" />}
           />
 
           {/* Care Agency Routes */}
           <Route
-            path="/careagencydashboard"
-            element={<Home initialUserType="care-agency" />}
-          />
-          <Route
-            path="/careagencydashboard/workers"
-            element={<Home initialUserType="care-agency" />}
-          />
-          <Route
-            path="/careagencydashboard/shifts"
-            element={<Home initialUserType="care-agency" />}
-          />
-          <Route
-            path="/careagencydashboard/incidents"
-            element={<Home initialUserType="care-agency" />}
-          />
-          <Route
-            path="/careagencydashboard/timesheets"
-            element={<Home initialUserType="care-agency" />}
-          />
-          <Route
-            path="/careagencydashboard/finance"
-            element={<Home initialUserType="care-agency" />}
-          />
-          <Route
-            path="/careagencydashboard/reports"
-            element={<Home initialUserType="care-agency" />}
-          />
-          <Route
-            path="/careagencydashboard/settings"
+            path="/careagencydashboard/*"
             element={<Home initialUserType="care-agency" />}
           />
 
           {/* Care Hub Routes */}
           <Route
-            path="/carehubdashboard"
-            element={<Home initialUserType="care-hub" />}
-          />
-          <Route
-            path="/carehubdashboard/post-shifts"
-            element={<Home initialUserType="care-hub" />}
-          />
-          <Route
-            path="/carehubdashboard/manage-shifts"
-            element={<Home initialUserType="care-hub" />}
-          />
-          <Route
-            path="/carehubdashboard/timesheet"
-            element={<Home initialUserType="care-hub" />}
-          />
-          <Route
-            path="/carehubdashboard/workers"
-            element={<Home initialUserType="care-hub" />}
-          />
-          <Route
-            path="/carehubdashboard/incidents"
-            element={<Home initialUserType="care-hub" />}
-          />
-          <Route
-            path="/carehubdashboard/reports"
-            element={<Home initialUserType="care-hub" />}
-          />
-          <Route
-            path="/carehubdashboard/invoice"
-            element={<Home initialUserType="care-hub" />}
-          />
-          <Route
-            path="/carehubdashboard/settings"
+            path="/carehubdashboard/*"
             element={<Home initialUserType="care-hub" />}
           />
         </Routes>
